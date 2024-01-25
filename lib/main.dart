265b55d3b3e7e05b02d75da7c2f63a5c4cb6e555
yewon <128056568+jeffyea320@users.noreply.github.com>
2024-01-26 01:06:08 +0900

    [front] 추천 만드는 중

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _getAppBar(),
-      extendBodyBehindAppBar: true,
+      extendBodyBehindAppBar: false,
       body: [home(), search(), community(), recommend()][tab],
       bottomNavigationBar: BottomNavigationBar(
         showUnselectedLabels: false,
