3d21c63526504200032cd4ca8b2fa0ec9d9b9d89
yewon <128056568+jeffyea320@users.noreply.github.com>
2024-01-29 14:06:42 +0900

    fix : 법률추천 커스텀위젯으로 뺌

--- lib/recommend.dart
+++ lib/recommend.dart
@@ -8,6 +8,11 @@ class recommend extends StatefulWidget {
   State<recommend> createState() => _recommendState();
 }
 
+bool _ison = false;
+List<String> title = ['1법률안','2법률안'];
+List<String> comment = ['1법률안 내용','2법률안 내용'];
+List<bool> help = [false,true];
+
 class _recommendState extends State<recommend> {
   bool button1State = false;
   bool button2State = false;
@@ -40,8 +45,6 @@ class _recommendState extends State<recommend> {
   bool button29State = false;
   bool button30State = false;
   bool button31State = false;
-
-  bool _ison = false;
 
   Future<void> _showDialog(BuildContext context) {
     return showDialog<void>(
@@ -700,8 +703,7 @@ class _recommendState extends State<recommend> {
         children: [
           SizedBox(height: 20,),
           Expanded(
-            child: SingleChildScrollView(
-                child: Column(
+            child: Column(
                   children: [
                     Container(
                       width: sizeX,
@@ -829,122 +831,105 @@ class _recommendState extends State<recommend> {
                         ),
                     ),
                     SizedBox(height: 20,),
-                    Container(
-                      width: sizeX,
-                      height: 430,
-                      color: Color(0xFFDEDEDE),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(15, 30, 15, 30),
-                        child: Container(
-                          width: sizeX*0.7,
-                          height: 190,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFE8E8E8),
-                            borderRadius: BorderRadius.circular(7),
-                            boxShadow:[
-                              BoxShadow(
-                                color: Colors.black12,
-                                spreadRadius: 2,
-                                offset: Offset(3,3),
-                              ),
-                            ],
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: Column(
-                            children: [
-                              Text("가나다라 법률안", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),textAlign: TextAlign.center),
-                              SizedBox(height: 30,),
-                              Text("안녕하십니까 저는 2학년 3반 권해진입니다. 제가 잘하는 것은 오래오래 잠자기이고요 그리고 머리를 대면 바로 잘 수 있습니다. 제가 못하는건 너무 많아서 나열할수가 없어요 죄송합니다. 가나다라 법률안은 이런 내용이에요 깜짝 놀라셨죠 어쩌라고요 그냥 해봤어요", style: TextStyle(fontSize: 20),),
-                              SizedBox(height: 10,),
-                              Container(
-                                height: 2,
-                                width: sizeX*0.85,
-                                color: Colors.white,
-                              ),
-                              SizedBox(height: 10,),
-                              Row(
-                                children: [
-                                  SizedBox(width: 170),
-                                  Text("법안 해석 도우미", style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
-                                  SizedBox(
-                                    height: 18,
-                                    child: CupertinoSwitch(
-                                        value: _ison,
-                                        activeColor: CupertinoColors.activeGreen,
-                                        onChanged: (bool value){
-                                          setState((){
-                                            _ison = value;
-                                          });
-                                        }),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20,),
-                    Container(
-                      width: sizeX,
-                      height: 430,
-                      color: Color(0xFFDEDEDE),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(15, 30, 15, 30),
-                        child: Container(
-                          width: sizeX*0.7,
-                          height: 190,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFE8E8E8),
-                            borderRadius: BorderRadius.circular(7),
-                            boxShadow:[
-                              BoxShadow(
-                                color: Colors.black12,
-                                spreadRadius: 2,
-                                offset: Offset(3,3),
-                              ),
-                            ],
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: Column(
-                            children: [
-                              Text("가나다라 법률안", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),textAlign: TextAlign.center),
-                              SizedBox(height: 30,),
-                              Text("안녕하십니까 저는 2학년 3반 권해진입니다. 제가 잘하는 것은 오래오래 잠자기이고요 그리고 머리를 대면 바로 잘 수 있습니다. 제가 못하는건 너무 많아서 나열할수가 없어요 죄송합니다. 가나다라 법률안은 이런 내용이에요 깜짝 놀라셨죠 어쩌라고요 그냥 해봤어요", style: TextStyle(fontSize: 20),),
-                              SizedBox(height: 10,),
-                              Container(
-                                height: 2,
-                                width: sizeX*0.85,
-                                color: Colors.white,
-                              ),
-                              SizedBox(height: 10,),
-                              Row(
-                                children: [
-                                  SizedBox(width: 170),
-                                  Text("법안 해석 도우미", style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
-                                  SizedBox(
-                                    height: 18,
-                                    child: CupertinoSwitch(
-                                        value: _ison,
-                                        activeColor: CupertinoColors.activeGreen,
-                                        onChanged: (bool value){
-                                          setState((){
-                                            _ison = value;
-                                          });
-                                        }),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                    Expanded(
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              padding: const EdgeInsets.all(10),
+                              itemCount: title.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return CustomContainer(
+                                  vtitle: title[index],
+                                  vcomment: comment[index],
+                                  vhelp: help[index],
+                                );
+                              },
+                            )
                     ),
                   ],
                 ),
               ),
-          ),
         ],
+      ),
+    );
+  }
+}
+
+
+class CustomContainer extends StatefulWidget {
+  final bool vhelp;
+  final String vtitle;
+  final String vcomment;
+
+  CustomContainer({
+    super.key,
+    required this.vhelp,
+    required this.vtitle,
+    required this.vcomment,
+  });
+
+  @override
+  State<CustomContainer> createState() => _CustomContainerState();
+}
+
+class _CustomContainerState extends State<CustomContainer> {
+  @override
+  Widget build(BuildContext context) {
+    final sizeX = MediaQuery.of(context).size.width;
+    final sizeY = MediaQuery.of(context).size.height;
+
+    return Container(
+      width: sizeX,
+      height: 430,
+      color: Color(0xFFDEDEDE),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 30, 15, 30),
+        child: Container(
+          width: sizeX*0.7,
+          height: 190,
+          decoration: BoxDecoration(
+            color: Color(0xFFE8E8E8),
+            borderRadius: BorderRadius.circular(7),
+            boxShadow:[
+              BoxShadow(
+                color: Colors.black12,
+                spreadRadius: 2,
+                offset: Offset(3,3),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Text(widget.vtitle, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),textAlign: TextAlign.center),
+              SizedBox(height: 30,),
+              Text(widget.vcomment, style: TextStyle(fontSize: 20),),
+              SizedBox(height: 10,),
+              Container(
+                height: 2,
+                width: sizeX*0.85,
+                color: Colors.white,
+              ),
+              SizedBox(height: 10,),
+              Row(
+                children: [
+                  SizedBox(width: sizeX*0.38),
+                  Text("법안 해석 도우미", style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
+                  SizedBox(
+                    height: 18,
+                    child: CupertinoSwitch(
+                        value: _ison,
+                        activeColor: CupertinoColors.activeGreen,
+                        onChanged: (bool value){
+                          setState((){
+                            _ison = value;
+                          });
+                        }),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
