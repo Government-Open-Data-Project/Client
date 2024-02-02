ee035fd759c6a81767f82d106a3e056c6c5d3ff3
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2024-02-03 00:30:30 +0900

    feat. 로그인 구현중

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
 
 class login extends StatefulWidget {
   login({Key? key}) : super(key: key);
@@ -8,6 +9,18 @@ class login extends StatefulWidget {
 }
 
 class _login extends State<login> {
+
+  void naverLogin() async{
+    //NaverLoginResult res = await FlutterNaverLogin.logIn();
+    final NaverLoginResult result = await FlutterNaverLogin.logIn();
+    NaverAccessToken res = await FlutterNaverLogin.currentAccessToken;
+    setState(() {
+      var accesToken = res.accessToken;
+      var tokenType = res.tokenType;
+      String name = result.account.name;
+    });
+    print("이름 : ${result.account.name}");
+  }
   @override
   Widget build(BuildContext context) {
     final sizeX = MediaQuery.of(context).size.width;
@@ -62,11 +75,40 @@ class _login extends State<login> {
               ),
             ),
             Center(
-                child: IconButton(
-              onPressed: () {},
-              icon: Image.asset('images/login/naverlogin.png'),
-              iconSize: 200,
-            )),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0,17,0,0),
+                  child: IconButton(
+                    onPressed: () async {
+                      try {
+                        final NaverLoginResult user = await FlutterNaverLogin.logIn();
+                        //NaverAccessToken res = await FlutterNaverLogin.currentAccessToken;
+
+                        print("error message : " + user.errorMessage);
+                        setState(() {
+                         /* var accessToken = res.accessToken;
+                          var tokenType = res.tokenType;
+                          var refreshToken = res.refreshToken;*/
+                        });
+
+                        String name = user.account.name;
+                        String tel = user.account.mobile
+                            .replaceAll('+82', '0')
+                            .replaceAll('-', '')
+                            .replaceAll(' ', '')
+                            .replaceAll('+', '');
+                        String sex = user.account.gender;
+                        String socialNo = '${user.account.birthyear}${user.account.birthday}'.replaceAll('-', '');
+
+                        print('$name, $tel, $sex, $socialNo');
+                      } catch (error) {
+                        print('Naver login error: $error');
+                      }
+                                      },
+                    icon: Image.asset('images/login/btnW.png'),
+                    iconSize: 200,
+                    style: ButtonStyle(),
+                  ),
+                )),
           ],
         ),
       ),
