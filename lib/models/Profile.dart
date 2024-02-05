e3916ff0a692bbc50898a2d1079a0196350a6f77
yewon <128056568+jeffyea320@users.noreply.github.com>
2024-02-05 17:18:56 +0900

    feat : 프로필 모델 생성

---
+++ lib/models/Profile.dart
@@ -0,0 +1,19 @@
+class Profile {
+  String name;
+  String age;
+  String isMarried;
+  String region;
+  String position;
+  List<String> interests;
+
+
+  Profile({
+    required this.name,
+    required this.age,
+    required this.isMarried,
+    required this.region,
+    required this.position,
+    required this.interests,
+
+  });
+}
